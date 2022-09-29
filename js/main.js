6973f2010f1b1950a0c540965ab4a561fd142821
Br1-NT <magaro.bruno@gmail.com>
2022-09-29 20:32:42 -0300

    widgets style

--- js/main.js
+++ js/main.js
@@ -1,7 +1,126 @@
+const NEGOCIOS = [
+    {
+    id: 0,
+    negocio: "Movil",
+    pcrc:"Black",
+    estado: false,
+    icon:"./assets/icon/icon-movil.svg",
+    color:"#50a9e9"
+    },
+    {
+    id: 1,    
+    negocio: "Movil",
+    pcrc:"Onboarding",
+    estado: false,
+    icon:"./assets/icon/icon-movil.svg",
+    color:"#50a9e9"
+    },
+    {
+    id: 2,
+    negocio: "Movil",
+    pcrc:"Abonos",
+    estado: false,
+    icon:"./assets/icon/icon-movil.svg",
+    color:"#50a9e9"
+    },
+    {
+    id: 3,    
+    negocio: "Movil",
+    pcrc:"Prepago",
+    estado: false,
+    icon:"./assets/icon/icon-movil.svg",
+    color:"#50a9e9"
+    },
+    {
+    id: 4,
+    negocio: "Cable",
+    pcrc:"Customer",
+    estado: false,
+    icon:"./assets/icon/care-cable.svg",
+    color:"#e95050"
+    },
+    {
+    id: 5,
+    negocio: "Cable",
+    pcrc:"Soporte",
+    estado: false,
+    icon:"./assets/icon/sop.svg",
+    color:"#e95050"
+    },
+    {
+    id: 6,
+    negocio: "Cable",
+    pcrc:"Uruguay",
+    estado: false,
+    icon:"./assets/icon/sop.svg",
+    color:"#e95050"
+    },
+    {
+    id: 7,
+    negocio: "Fija",
+    pcrc:"Customer",
+    estado: false,
+    icon:"./assets/icon/care-fija.svg",
+    color:"#e9d750"
+    },
+    {
+    id: 8,
+    negocio: "Fija",
+    pcrc:"Soporte",
+    estado: false,
+    icon:"./assets/icon/sop.svg",
+    color:"#e9d750"
+    }
+];
+
 const nav = document.querySelector('.nav');
 const btnNav = document.querySelector('#btn-nav');
-console.log(btnNav);
+const dashboard = document.querySelector('.dashboard');
 
 btnNav.addEventListener('click', () => {
     nav.classList.toggle('nav-off');
+});
+
+//Render Widgets
+const renderWidgets = () => {
+    for( pcrc of NEGOCIOS){
+        dashboard.innerHTML += `
+            <div class="widget ${pcrc.negocio}" id="${pcrc.id}">
+                <div class="widget__span" style="border-right: 10px solid ${pcrc.color};">
+                    <h4>${pcrc.negocio}</h3>
+                </div>
+                <div class="widget__title">
+                    <img src="${pcrc.icon}" alt="" class="widget__icon"><h2>${pcrc.pcrc}</h2>
+                </div>
+                <div class="widget__status">
+                    <p>Estato: ${(pcrc.estado) ? "Activado":"Desactivado"} </p> 
+                    </div>
+                <button class="widget__btn">Activar</button>
+                <div class="widget__data">
+                    <p>Ultima modificación: - </p>
+                </div>
+            </div>
+        `;
+    };
+};
+renderWidgets();
+
+let widgets = document.querySelectorAll('.widget'); 
+
+nav.addEventListener('click', elem => {
+    if(elem.target.classList.contains('negocio') || elem.target.parentElement.classList.contains('negocio')){
+        document.querySelectorAll('.widget').forEach(e => {
+            if (!e.classList.contains(elem.target.firstChild.textContent)) { 
+                e.classList.add('oculto');
+            } else {
+                e.classList.remove('oculto');
+            }
+        });
+    } else if(elem.target.classList.contains('ot') || elem.target.parentElement.classList.contains('ot')){
+        document.querySelectorAll('.widget').forEach(e => {
+            if (e.classList.contains('oculto')) { 
+                e.classList.remove('oculto');
+            }
+        });
+    };
 });
